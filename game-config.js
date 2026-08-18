8298360b9f0fa12b4115a4fd8b4daf8c60b6c34a
HALGROUP <gaias.hirokuni@gmail.com>
2026-08-19 01:13:43 +0900

    Add files via upload

---
+++ game-config.js
@@ -0,0 +1,8 @@
+window.GORILLA_CONFIG = {
+  storageKey:"kunioStoryEndsV2", saveKey:"kunioStorySaveV2", seenKey:"kunioStorySeenV2", settingsKey:"kunioStorySettingsV2",
+  xUrl:"https://x.com/EVILKING_KRKS",
+  scoreKeys:["love","trust","chaos","dark","gorilla","muscle"],
+  endingOrder:["normal","muscle","pokeca","love","armLove","dark","collar","true"],
+  endingCG:{normal:"normal.webp",muscle:"muscle.webp",pokeca:"pokeca.webp",love:"love.webp",armLove:"arm-love.webp",dark:"dark.webp",collar:"collar.webp",true:"true.webp"},
+  defaultSettings:{textSpeed:26,autoDelay:1150,bgmVolume:60,seVolume:70}
+};
